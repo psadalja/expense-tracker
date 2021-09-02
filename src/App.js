235325a08315a95cf1b6c4c98c66a8e0490b86f1
psadalja <parthadalja29@gmail.com>
2021-09-03 00:27:00 +0530

    add expanse item coponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ExpenseItem from "./components/ExpenseItem";
 
 const App = () => {
   return (
     <div>
       <h2>Lets Get Started</h2>
       <p>this is not visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 };
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>Septeber 3rd 2021</div>
+      <div>
+        <h1>Nachos</h1>
+        <div>85</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
