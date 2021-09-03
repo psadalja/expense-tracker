0bb67893868741f44228f117c32e6a7df32bc82c
psadalja <parthadalja29@gmail.com>
2021-09-03 15:50:23 +0530

    Add multiple component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 2, 28);
-  const expanseName = "Nachos";
-  const expanseAmount = 85;
-
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expanseName}</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">{expanseAmount}</div>
+      <div className="expense-item__price">{props.amount}</div>
     </div>
   );
 };
