11fb9e59773e17aff47cb97caf2455efa22750be
psadalja <parthadalja29@gmail.com>
2021-09-05 17:24:17 +0530

    updated file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
 
 const App = () => {
   const expenses = [
