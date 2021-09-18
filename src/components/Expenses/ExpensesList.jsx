057f95a62a61d3776168c3d8fac464321393e602
psadalja <parthadalja29@gmail.com>
2021-09-18 13:33:03 +0530

    add expenses list component

---
+++ src/components/Expenses/ExpensesList.jsx
@@ -0,0 +1,23 @@
+import ExpenseItem from "./ExpenseItem";
+import Expenses from "./Expenses";
+import "./ExpensesList.css";
+
+const ExpenseList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
+  }
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expenses) => (
+        <ExpenseItem
+          key={expenses.id}
+          title={expenses.title}
+          amount={expenses.amount}
+          date={expenses.date}
+        />
+      ))}
+      ;
+    </ul>
+  );
+};
+export default ExpenseList;
